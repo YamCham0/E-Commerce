d01076849d14d4a0df04f2861e32bd3784a4f89a
Yamil Chamo <yajochakha@gmail.com>
2021-10-28 21:21:15 -0500

    initial commit

---
+++ models/index.js
@@ -0,0 +1,30 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
+// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Product,{
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
